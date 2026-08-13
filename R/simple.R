54474b16bdbd715648020a7f2f02a4ae57f27335
Ouma Ronald <ronald2ouma2@gmail.com>
2026-08-13 15:54:47 +0300

    Documentation Improvement

--- R/simple.R
+++ R/simple.R
@@ -17,8 +17,8 @@ MPA.REGEXP <- "^[kpcofgst]__"
 #' Subset a data.frame of signatures by curator
 #'
 #' @param dat data.frame produced by \link[bugsigdbr]{importBugSigDB}
-#' @param curator curator to subset by
-#' @param curator.column name of column of curators in data.frame
+#' @param curator character vector of one or more curator names to subset by
+#' @param curator.column name of column of curators in data.frame (default: "Curator")
 #'
 #' @return data.frame subsetted by curator
 #' @importFrom dplyr filter %>%
@@ -51,15 +51,18 @@ subsetByCurator <- function(dat, curator, curator.column="Curator")
 
 #' Get the most frequently occurring taxa in a table of signatures
 #' @param dat A table such as output by \link[bugsigdbr]{importBugSigDB}
-#' @param n Number of most frequently occurring taxa to return
-#' @param sig.type increased for increased in cases relative to controls, decreased for decreased in cases relative to controls, both for either
-#' @param direction.column column containing direction information in dat
+#' @param n Number of most frequently occurring taxa to return (default: 10)
+#' @param sig.type "increased" for taxa increased in cases relative to controls, "decreased" for decreased, "both" for either (default: "both")
+#' @param direction.column column containing direction information in dat (default: "Abundance in Group 1")
 #' @importFrom dplyr filter %>%
 #' @importFrom bugsigdbr getSignatures
 #' @export
-#' @return 
-#' A list of signatures, with PMIDs for list element names 
-#' @examples 
+#' @return
+#' A named table of taxon counts (as produced by \code{table}), sorted in
+#' decreasing order. Names are the full metaphlan-style taxon path strings
+#' (e.g. "k__Bacteria|g__Blautia", not just the tip taxon), and values are
+#' their frequency across signatures in \code{dat}.
+#' @examples
 #' full.dat <- bugsigdbr::importBugSigDB()
 #' getMostFrequentTaxa(full.dat)
 getMostFrequentTaxa <- function(dat, n=10, sig.type=c("both", "increased", "decreased"), direction.column="Abundance in Group 1")
@@ -76,18 +79,23 @@ getMostFrequentTaxa <- function(dat, n=10, sig.type=c("both", "increased", "decr
 }
 
 
-#' Author: Giacomo Antonello
-#' Date: 2025-03-17
-#' 
-#' Description:
-#' 
-#' This function takes a raw bugSigDB input from `bugsigdbr` and generates a 
-#' unique idenfier as curatedMetagenomicsData does: full last name, initial(s) of 
-#' first name(s) and year of publication. Additionally, it checks if there are 
-#' more PMID codes associated with the same ID and adds a .1, .2, for each 
-#' duplication
-#' 
+#' Build unique study identifiers for a table of signatures
+#'
+#' Generates a study identifier the way curatedMetagenomicData does it: the
+#' first author from the \code{Authors list} column (usually surname +
+#' initials, e.g. "ZellerG") with whitespace stripped, plus the year, e.g.
+#' "ZellerG_2014". Duplicates get a ".1", ".2", ... suffix. Also normalizes
+#' bare DOIs (starting with "10.") to full \url{https://doi.org/} links.
+#'
 #' @param bsdb.df \code{data.frame} produced by \link[bugsigdbr]{importBugSigDB}, pre-filtered as desired
+#'
+#' @return \code{bsdb.df} with a new leading \code{Study Identifier} column
+#' @note Author: Giacomo Antonello (2025-03-17)
+#' @keywords internal
+#'
+#' @examples
+#' full.dat <- bugsigdbr::importBugSigDB()
+#' bugSigSimple:::.make_unique_study_ID(full.dat[1:10, ])
 
 .make_unique_study_ID <- function(bsdb.df){
   bsdb_with_StudyIDs <- bsdb.df %>% 
